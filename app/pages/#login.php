4bd589e2b8378496a7cbe035d5c53bb65c243a34
SamoSadlaker <samkosadlak@gmail.com>
2021-03-24 22:32:53 +0100

    Login

--- app/pages/#login.php
+++ app/pages/#login.php
@@ -8,8 +8,8 @@ $auth->aLoged();
       <img src="assets/img/logo.svg" alt="Logo">
       <h1>Login page</h1>
     </div>
-    <form action="/core/login.php" method="POST">
-
+    <form action="/core/login.php" method="POST" id="loginForm">
+      <p id="error"></p>
       <div class="line">
         <label for="email">Email</label>
         <input type="email" name="email" id="email" required placeholder="your email">
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
+// Popalert
+function popalert(type, message) {
+    var icon = "info";
+
+    if (type == "success") {
+        icon = "check";
+    }
+    if (type == "error") {
+        icon = "error";
+    }
+
+    var alert = `<div id="alert" class="alert-${type}"><i class='bx bxs-${icon}-circle'></i>${message}</div>`;
+
+    // $("main").append(alert);
+    document.body.innerHTML += alert;
+
+    setTimeout(() => {
+        document.getElementById("alert").remove();
+    }, 8000);
+}
+
+// Sidebar
 var sidebar = document.getElementById("Sidebar");
 var menu = document.getElementById("menu");
 var main = document.getElementById("Main");
@@ -6,5 +28,42 @@ if (menu && sidebar && main) {
     menu.addEventListener("click", () => {
         sidebar.classList.toggle("mini");
         main.classList.toggle("max");
+    });
+}
+
+// Login form
+var login = document.getElementById("loginForm");
+var error = document.getElementById("error");
+
+if (login && error) {
+    login.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST", login.getAttribute("action"), true);
+        xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+        var formdata = new FormData();
+        formdata.append("email", document.getElementById("email").value);
+        formdata.append("password", document.getElementById("password").value);
+
+        xmlhttp.onreadystatechange = () => {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var res = JSON.parse(xmlhttp.responseText);
+
+                console.log(res.status);
+                if (res.status == "validate") {
+                    error.textContent = "* " + res.message;
+                }
+                if (res.status == "error") {
+                    error.textContent = "";
+                    popalert("error", res.message);
+                }
+                if (res.status == "success") {
+                    window.location.replace("/");
+                }
+            }
+        };
+        xmlhttp.send(formdata);
     });
 }
