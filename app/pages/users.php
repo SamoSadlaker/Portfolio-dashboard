f7b2ec5a490ac0aece5bf63c7c62c6a527cbe782
SamoSadlaker <samkosadlak@gmail.com>
2021-04-01 19:09:44 +0200

    Users

--- app/pages/users.php
+++ app/pages/users.php
@@ -1,1 +1,42 @@
-users
+<div id="Users">
+  <h2 class="title">Users</h2>
+
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>UUID</th>
+        <th>Name</th>
+        <th>Lastname</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Position</th>
+        <th>Verified</th>
+      </tr>
+    </thead>
+    
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>#adbc12</td>
+        <td>Samuel</td>
+        <td>Šadlák</td>
+        <td>samosadlaker</td>
+        <td>dev@samosadlaker.eu</td>
+        <td>Owner</td>
+        <td><i class='bx bxs-check-circle true'></i></td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>#adbc12</td>
+        <td>Samuel</td>
+        <td>Šadlák</td>
+        <td>samosadlaker</td>
+        <td>dev@samosadlaker.eu</td>
+        <td>Owner</td>
+        <td><i class='bx bx-error false'></i></td>
+      </tr>
+    </tbody>
+  </table>
+
+</div>
