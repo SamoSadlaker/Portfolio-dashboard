f878ff401eac5af1db158fb0b15349af9df39ba8
SamoSadlaker <samkosadlak@gmail.com>
2021-04-05 19:55:23 +0200

    Orders

--- app/pages/orders.php
+++ app/pages/orders.php
@@ -1,1 +1,16 @@
-orders
+<div id="Orders">
+  <h2 class="title">Orders</h2>
+
+  <div class="orders-container">
+    <div class="card">
+      <h3>Name</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita facere</p>
+      <div class="bar">
+        <p class="value">67%</p>
+        <div class="progres" style="width: 67%;"></div>
+      </div>
+    </div>
+
+  </div>
+
+</div>
