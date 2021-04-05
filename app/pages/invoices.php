e2fec0a5ac8e05eda9c2e89c21b9f59632648d68
SamoSadlaker <samkosadlak@gmail.com>
2021-04-05 19:24:43 +0200

    Invoices

--- app/pages/invoices.php
+++ app/pages/invoices.php
@@ -1,1 +1,22 @@
-invoices
+<div id="Invoices">
+  <h2 class="title">Invoices</h2>
+
+
+  <div class="invoice-container">
+    <div class="card done">
+      <i class='bx bxs-file-pdf'></i>
+      <p>Order #1</p>
+      <a href="#">View</a>
+      <time>15.3.2021 - 15:30</time>
+    </div>
+    <div class="card">
+      <i class='bx bxs-file-pdf'></i>
+      <p>Order #2</p>
+      <a href="#">View</a>
+      <time>28.3.2021 - 17:50</time>
+    </div>
+    
+  </div>
+ 
+
+</div>
