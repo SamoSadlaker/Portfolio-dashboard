35296eab170364acc284ed346c52b2a508011942
SamoSadlaker <samkosadlak@gmail.com>
2021-07-07 16:21:07 +0200

    test

--- core/test.php
+++ core/test.php
@@ -1,2 +1,4 @@
 <?php
-var_dump(strcasecmp("dev@samosadlaker.eu", "dev@samosadlaker.eu") == 0);
+define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define("APP_ROOT", ROOT . "app" . DIRECTORY_SEPARATOR);
+var_dump(APP_ROOT);
